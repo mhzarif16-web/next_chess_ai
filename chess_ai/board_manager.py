c6db1e0676c276620ea3e7c6b5edcb6fcd1806e9
mhzarif16-web <mhzarif16@gmail.com>
2025-08-20 19:29:41 +0600

    Update board_manager.py

--- chess_ai/board_manager.py
+++ chess_ai/board_manager.py
@@ -1,1 +1,20 @@
+import chess
 
+class BoardManager:
+    def __init__(self):
+        self.board = chess.Board()
+
+    def show_board(self):
+        print(self.board)
+
+    def get_legal_moves(self):
+        return list(self.board.legal_moves)
+
+    def make_move(self, move):
+        self.board.push(move)
+
+    def get_fen(self):
+        return self.board.fen()
+
+    def game_over(self):
+        return self.board.is_game_over()
